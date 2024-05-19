888a03f37599782a0076273df0cfc42412c6cc7b
Apratim Jaiswal <apratim99@gmail.com>
2024-05-20 00:08:26 +0530

    Update 5_balanced_parantheses.js

--- 2_Strings/5_balanced_parantheses.js
+++ 2_Strings/5_balanced_parantheses.js
@@ -2,25 +2,24 @@
  * Check if BALANCED PARANTHESES
  */
 
-const checkBalancedParanthese = (a) => {
+function check(a) {
   let stack = [];
+
   for (let i = 0; i < a.length; i++) {
     if (a[i] == "(") {
-      // "("
       stack.push(a[i]);
     } else {
-      // ")"
-      if (stack[stack.length - 1] !== "(") {
-        return false;
+      let topElem = stack.pop();
+      if (topElem !== "(") {
+        console.log("Not balanced");
+        return;
       }
-      stack.pop();
     }
   }
-  if (stack.length == 0) return true;
-  return false;
-};
+
+  if (stack.length == 0) console.log("Balanced");
+  else console.log("Not balanced");
+}
 
 let a = "(()())";
-checkBalancedParanthese(a)
-  ? console.log("Balanced parantheses")
-  : console.log("Not balanced parantheses");
+check(a);
